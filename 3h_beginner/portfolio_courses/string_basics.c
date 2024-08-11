81a23e648bc75a1d218184b03b8bab8e767e7866
Mwakanemela <kayangemwakanemela@gmail.com>
2024-08-11 17:44:46 -0400

    Counting total number of a's that exist in myName variable

--- 3h_beginner/portfolio_courses/string_basics.c
+++ 3h_beginner/portfolio_courses/string_basics.c
@@ -9,5 +9,15 @@ int main()
  int length = strlen(myName);
 
  printf("String length is: %d\n", length);
+
+ int count_a = 0;
+
+ for(int i = 0; i < length; i++)
+ {
+  if(myName[i] == 'a' || myName[i] == 'A')
+    count_a++;
+ }
+
+ printf("a count is %d\n", count_a);
  return 0;
 }
