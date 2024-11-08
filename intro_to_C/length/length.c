db3ffa6e087b5bdc94ec8dcaa9f4c561baad0325
Mwakanemela <kayangemwakanemela@gmail.com>
2024-11-08 08:03:07 -0500

    a program to read a line from keyboard and report its length

---
+++ intro_to_C/length/length.c
@@ -0,0 +1,25 @@
+/*******************************
+
+- length.c : reads a line from keyboard and reports its length 
+	     -it reads including spaces and add 1 for end-of-string char = '\0'
+
+- Author   : Mwakanemela Kayange
+
+- Usage:   : gcc length.c  -o length
+		./length
+*******************************/
+
+
+#include <stdio.h>
+#include <string.h>
+
+char line[100]; /* line to read and print its length */
+
+int main() {
+
+	printf("Enter a line: ");
+	fgets(line, sizeof(line), stdin);
+
+	printf("The length of the line is: %d\n", strlen(line));
+	return 0;
+}
